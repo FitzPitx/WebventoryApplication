07d52feebe6e4cbecb13a0ab817669fa6c514c0a
FitzPitx <anieto@uniempresarial.edu.co>
2023-09-11 21:00:27 -0500

    edit sql

--- database/db.js
+++ database/db.js
@@ -1,10 +1,17 @@
-const mysql = require('mysql');
-const conexion = mysql.createConnection({
-    host: 'localhost',
-    password: '',
-    user: 'root',
-    database: 'inventario'
-});
+const mysql = require('mssql');
+const conexion = {
+    user: 'andresAdmin', // better stored in an app setting such as process.env.DB_USER
+    password: 'Sandia2408$!', // better stored in an app setting such as process.env.DB_PASSWORD
+    server: 'mysqlserverandrew2408.database.windows.net', // better stored in an app setting such as process.env.DB_SERVER
+    port: 1433, // optional, defaults to 1433, better stored in an app setting such as process.env.DB_PORT
+    database: 'inventario', // better stored in an app setting such as process.env.DB_NAME
+    authentication: {
+        type: 'default'
+    },
+    options: {
+        encrypt: true
+    }
+}
 
 conexion.connect((err)=>{
     if(err){
